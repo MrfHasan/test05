17a4261e99228f3b284e11e303bfe43464a975e9
Maruf <mrfhasan01@gmail.com>
2023-04-05 10:52:25 +0600

    6th Commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    message = "Hello, GIM! Hello People, What's Going on? free or work on..! Hope everything goes well."
+    message = "Hello, GIM! Hello People, What's Going on? free or work on..! Hope everything goes well. We will meet soon"
     return {
         'statusCode': 200,
         'body': json.dumps(message)
